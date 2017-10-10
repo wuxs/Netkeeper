8a11ed61534da82fdb8385ce6c8e0bbb23181e3c
1941474711 <949625012@qq.com>
2017-10-10 17:05:06 +0800

    Update settings.py

--- netkeeper/settings.py
+++ netkeeper/settings.py
@@ -6,7 +6,7 @@ LOG_FILE = OUTPUT_PATH + os.path.sep + "service.log"
 RECORD_FILE = OUTPUT_PATH + os.path.sep + "record.txt"
 CONFIG_FILE = OUTPUT_PATH + os.path.sep + "config.conf"
 
-# USERNAME = "17702737125@hkd"
-# PASSWORD = "09152287"
+# USERNAME = "xxxxx@hkd"
+# PASSWORD = "xxxxx"
 
 RUNNING = False
